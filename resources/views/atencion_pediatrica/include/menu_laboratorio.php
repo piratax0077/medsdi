31542734359d2c288218d351d5e55e6b63d59424
Johan <j2edavila@gmail.com>
2023-02-16 02:12:34 -0300

    pediatria jaime

---
+++ resources/views/atencion_pediatrica/include/menu_laboratorio.php
@@ -0,0 +1,61 @@
+<nav class="pcoded-navbar menu-light">
+	<div class="navbar-wrapper">
+		<div class="navbar-content scroll-div">
+			<div class="">
+				<div class="main-menu-header">
+					<img class="img-radius" src="../assets/images/iconos/usuario_laboratorio.svg" alt="Laboratorio">
+					<div class="user-details">
+						<div id="more-details">Nombre Laboratorio <i class="fa fa-caret-down"></i></div>
+					</div>
+				</div>
+				<div id="nav-user-link">
+					<ul class="list-inline">
+						<li class="list-inline-item">
+							<a href="perfil_laboratorio.php" data-toggle="tooltip" title="Mi perfil">
+								<i class="feather icon-user"></i>
+							</a>
+						</li>
+						<li class="list-inline-item"><a href="cerrar.php" data-toggle="tooltip" title="Cerrar sesión" class="text-danger"><i class="feather icon-power"></i></a></li>
+					</ul>
+				</div>
+			</div>
+			<ul class="nav pcoded-inner-navbar ">
+				<li class="nav-item pcoded-menu-caption text-center">
+				</li>
+				<li class="nav-item pcoded-hasmenu">
+					<a href="javascript:void(0)" class="nav-link"><span class="pcoded-micon"><i class="feather icon-home"></i></span><span class="pcoded-mtext text-center">Mi Escritorio</span></a>
+					<ul class="pcoded-submenu">
+						<li>
+							<a href="escritorio_laboratorio.php">Mi Escritorio </a>
+						</li>
+						<li>
+							<a href="#">Mis Agenda</a>
+						</li>
+						<li>
+							<a href="../escritorio_laboratorio/pacientes_laboratorio.php">Mis Pacientes</a>
+						</li>
+						<li>
+							<a href="../escritorio_laboratorio/recepcion_muestras.php">Recepción de Muestras</a>
+						</li>
+						<li><a href="../escritorio_laboratorio/configuraciones_laboratorio.php">
+						Panel de Configuración</a></li>
+						<li>
+							<a href="../receta_online_Laboratorio/inicio_receta_online_laboratorio.php">Receta Online</a>
+						</li>
+					</ul>
+				</li>
+				<li class="nav-item pcoded-hasmenu">
+					<a href="javascript:void(0)" class="nav-link"><span class="pcoded-micon"><i class="feather feather icon-menu"></i></span><span class="pcoded-mtext text-center">Otras opciones</span></a>
+					<ul class="pcoded-submenu">
+						<li>
+							<a href="perfil_laboratorio.php">Mi Perfil</a>
+						</li>
+						<li>
+							<a href="suscripcion_pago_laboratorio.php">Suscripción y Pagos</a>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
