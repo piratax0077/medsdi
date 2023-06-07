728f4fdcb9a7fae4c8303b08b35f47fe35011811
dasebra <90878769+dasebra@users.noreply.github.com>
2023-06-06 21:01:43 -0400

    Home adm_cm

--- resources/views/app/adm_cm/home.blade.php
+++ resources/views/app/adm_cm/home.blade.php
@@ -24,24 +24,24 @@
             <!--Cierre: Header-->
             <!--Botones-->
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
                     <div class="card subir py-auto bg-info">
                         <div class="card-body text-center">
-                            <h5 class=" mb-0 text-white f-24">Centro médico {{ mb_strtoupper($institucion->nombre) }}</h5>
+                            <h5 class=" mb-0 text-white f-20">Centro médico {{ mb_strtoupper($institucion->nombre) }}</h5>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
                     <div class="card subir py-auto">
                         <a href="{{ ROUTE('adm_cm.configuracion') }}">
                             <div class="card-body text-center" style="cursor:pointer">
-                                <img class="wid-60 text-center" src="{{ asset('images/iconos/perfiles.svg') }}">
-                                <h5 class="mt-1 mb-0">Configurar Mi Centro</h5>
+                                <img class="wid-60 text-center" src="{{ asset('images/iconos/panel_configuracion.svg') }}">
+                                <h5 class="mt-1 mb-0">Configurar mi Centro</h5>
                             </div>
                         </a>
                     </div>
                 </div>
-				<div class="col-md-4">
+				<div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
                     <div class="card subir py-auto">
                         <a href="{{ ROUTE('adm_cm.adm_medico') }}">
                             <div class="card-body text-center" style="cursor:pointer">
@@ -51,27 +51,27 @@
                         </a>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
                     <div class="card subir py-auto">
                         <a href="{{ ROUTE('adm_cm.administracion_cm') }}">
                             <div class="card-body text-center" style="cursor:pointer">
-                                <img class="wid-60 text-center" src="{{ asset('images/iconos/administracion.svg') }}">
+                                <img class="wid-60 text-center" src="{{ asset('images/iconos/adm_comercial.png') }}">
                                 <h5 class="mt-1 mb-0">Administración comercial</h5>
                             </div>
                         </a>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
                     <div class="card subir py-auto">
                         <a href="{{ ROUTE('adm_cm.profesionales') }}">
                             <div class="card-body text-center" style="cursor:pointer">
-                                <img class="wid-60 text-center" src="{{ asset('images/iconos/profesional_2.svg') }}">
+                                <img class="wid-60 text-center" src="{{ asset('images/iconos/profesional.svg') }}">
                                 <h5 class="mt-1 mb-0">Profesionales</h5>
                             </div>
                         </a>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
                     <div class="card subir py-auto">
                         <a href="{{ ROUTE('adm_cm.pacientes') }}">
                             <div class="card-body text-center" style="cursor:pointer">
@@ -81,56 +81,59 @@
                         </a>
                     </div>
                 </div>
-				 <div class="col-md-4">
+				<div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
                     <div class="card subir py-auto">
                         <a href="{{ ROUTE('adm_cm.personal') }}">
                             <div class="card-body text-center" style="cursor:pointer">
-                                <img class="wid-60 text-center"  src="{{ asset('images/iconos/equipo.svg') }}">
+                                <img class="wid-60 text-center"  src="{{ asset('images/iconos/personal.png') }}">
                                 <h5 class="mt-1 mb-0">Personal del Centro</h5>
                             </div>
                         </a>
                     </div>
                 </div>
 
-                <div class="col-md-12">
+                <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
                     <div class="card subir py-auto bg-warning">
                         <div class="card-body text-center" style="cursor:pointer">
-                            <h5 class=" mb-0 text-white f-24">Areas del centro médico</h5>
+                            <h5 class=" mb-0 text-white f-20">Areas del centro médico</h5>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
                     <div class="card subir py-auto" onclick="en_construccion()";>
                       {{--  <a href="{{ ROUTE('adm_cm.laboratorio') }}"></a>--}}
                             <div class="card-body text-center" style="cursor:pointer">
+                            <img class="wid-70 text-center" src="{{ asset('images/iconos/laboratorio.svg') }}">
                                 <h5 class="mt-1 mb-0">Laboratorio</h5>
                             </div>
                         </a>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
                     <div class="card subir py-auto" onclick="en_construccion()";>
                       {{-- <a href="{{ ROUTE('adm_cm.laboratorio') }}"></a>--}}
                             <div class="card-body text-center" style="cursor:pointer">
+                            <img class="wid-70 text-center" src="{{ asset('images/iconos/imagenologia.svg') }}">
                                 <h5 class="mt-1 mb-0">Imagenología</h5>
                             </div>
                         </a>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
                     <div class="card subir py-auto" onclick="en_construccion()";>
                        {{-- <a href="{{ ROUTE('adm_cm.vacunatorio') }}"></a>--}}
                             <div class="card-body text-center" style="cursor:pointer">
+                            <img class="wid-70 text-center" src="{{ asset('images/iconos/vacunatorio.svg') }}">
                                 <h5 class="mt-1 mb-0">Vacunatorio</h5>
                             </div>
                         </a>
                     </div>
                 </div>
-				<div class="col-md-12">
+				<div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
 					<div class="card subir py-auto" onclick="en_construccion()";>
 						<a href="#">
 							<div class="card-body text-center" style="cursor:pointer">
-								<img class="wid-70 text-center" src="{{ asset('images/iconos/usuario_asistente.svg') }}">
+								<img class="wid-70 text-center" src="{{ asset('images/iconos/mis_asistentes.svg') }}">
 								<h5 class="mt-1">Contratar Asistentes</h5>
 							</div>
 						</a>
