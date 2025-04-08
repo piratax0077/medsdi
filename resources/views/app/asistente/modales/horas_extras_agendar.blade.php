e05747f27af96a6068a6f2e9b42b9b9ed98d31ed
piratax0077 <francisco.rojo.gallardo@gmail.com>
2025-04-08 18:46:19 -0400

    fix horas extras asistente agenda profesional

--- resources/views/app/asistente/modales/horas_extras_agendar.blade.php
+++ resources/views/app/asistente/modales/horas_extras_agendar.blade.php
@@ -421,6 +421,7 @@
         let id_asistente = $('#he_reserva_hora_id_asistente').val();
         let origen = $('#he_reserva_hora_origen').val();
         let tipo_agenda = $('#id_tipo_agenda').val();
+        let id_paciente = $('#he_reserva_hora_id_paciente').val();
         var tipo_agenda_text = 'C';
 
         switch (tipo_agenda) {
@@ -449,13 +450,14 @@
                     id_profesional: id_profesional,
                     id_asistente: id_asistente,
                     origen: origen,
+                    id_paciente: id_paciente,
                     tipo_hora_medica: tipo_agenda_text,
                 }
             })
             .done(function(data) {
                 if (data != null) {
 
-                    data = JSON.parse(data);
+                    // data = JSON.parse(data);
                     if(data.estado == 'error')
                     {
                         swal({
